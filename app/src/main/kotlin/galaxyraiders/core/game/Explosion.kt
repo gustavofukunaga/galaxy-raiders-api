d19a8cd4b26d83aa6f2796458be23f13082d60a3
Gustavo Ueti Fukunaga <gustavofukunaga@usp.br>
2023-06-20 10:48:42 -0300

    Update Explosion.kt

--- app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
+++ app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
@@ -4,10 +4,9 @@ import galaxyraiders.core.physics.Point2D
 import galaxyraiders.core.physics.Vector2D
 
 class Explosion(
-  initialPosition: Point2D,
-  initialVelocity: Vector2D,
-  radius: Double,
-  mass: Double
-  is_triggered: Boolean
-) :
-  SpaceObject("Explosion", '*', initialPosition, initialVelocity, radius, mass)
+    initialPosition: Point2D,
+    initialVelocity: Vector2D,
+    radius: Double,
+    mass: Double,
+// is_triggered: Boolean
+) : SpaceObject("Explosion", '*', initialPosition, initialVelocity, radius, mass)
