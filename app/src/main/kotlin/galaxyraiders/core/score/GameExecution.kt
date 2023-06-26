3f0cffcc793fa8f60eadb16852998f22d3ec0794
Gustavo Ueti Fukunaga <gustavofukunaga@usp.br>
2023-06-26 16:18:38 -0300

    Update GameExecution.kt
    Added addToLeaderboard() that creates Leaderboard.json with 3 highest scored executions from Scoreboard.json
    
    Code could be cleaned up with a json library

--- app/src/main/kotlin/galaxyraiders/core/score/GameExecution.kt
+++ app/src/main/kotlin/galaxyraiders/core/score/GameExecution.kt
@@ -1,5 +1,6 @@
 package galaxyraiders.core.score
 import java.io.File
+import java.time.LocalDateTime
 
 data class GameExecution(
   val startTime: String,
@@ -62,5 +63,52 @@ data class GameExecution(
   fun updateScore(newScore: Double, newDestroyedAsteroids: Int) {
     this.score = newScore
     this.destroyedAsteroids = newDestroyedAsteroids
+  }
+
+  fun addToLeaderboard() {
+    val scoreFile = File("src/main/kotlin/galaxyraiders/core/score/Scoreboard.json")
+    val scores = scoreFile.readText()
+    
+    val gameDataList = parseJson(scores)
+
+    val sortedList = gameDataList.sortedByDescending { it.score }
+    val sortedTop3 = sortedList.subList(0, 3)
+
+    var content = "["
+    for (gameData in sortedTop3) {
+        content += "{\"startTime\": \"${gameData.startTime}\",  \"Score\": ${gameData.score}"
+        content += ", \"destroyedAsteroids\": ${gameData.destroyedAsteroids}}, "
+    }
+
+    val newContent = content.substring(0, content.lastIndexOf(",")) + "]"
+
+    val leaderboardFile = File("src/main/kotlin/galaxyraiders/core/score/Leaderboard.json")
+    leaderboardFile.createNewFile()
+    leaderboardFile.writeText(newContent)
+  }
+
+  data class GameData(val startTime: String, var score: Double, var destroyedAsteroids: Int)
+  
+  fun parseJson(json: String): List<GameData> {
+    val gameDataList = mutableListOf<GameData>()
+
+    val jsonArray = json.trim().removeSurrounding("[", "]").split("},")
+    for (item in jsonArray) {
+        val trimmedItem = item.trim().removeSurrounding("{", "}")
+        val fields = trimmedItem.split(",")
+        val startTime = extractFieldValue(fields[0]).substring(1)
+        val score = extractFieldValue(fields[1])
+        val destroyedAsteroids = extractFieldValue(fields[2])
+
+        val gameData = GameData(startTime, score.toDouble(), destroyedAsteroids.toInt())
+        gameDataList.add(gameData)
+    }
+
+    return gameDataList
+  }
+
+  fun extractFieldValue(field: String): String {
+      val fieldValue = field.split(":")[1]
+      return fieldValue.trim().removeSurrounding("\"")
   }
 }
