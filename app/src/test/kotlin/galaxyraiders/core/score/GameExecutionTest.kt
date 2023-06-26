8b9daad5b1d588b6f65b4cde9fc67cbccd4b1164
Gustavo Ueti Fukunaga <gustavofukunaga@usp.br>
2023-06-26 03:15:51 -0300

    Create GameExecutionTest.kt

---
+++ app/src/test/kotlin/galaxyraiders/core/score/GameExecutionTest.kt
@@ -0,0 +1,48 @@
+package galaxyraiders.core.score
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+import kotlin.test.assertTrue
+
+import java.time.LocalDateTime
+import java.io.File
+
+@DisplayName("Given an game execution")
+class GameExecutionTest {
+  private val execution = GameExecution(
+    LocalDateTime.now().toString(),
+    0.0,
+    0
+  )
+
+  @Test
+  fun `it updates score GameExecution `() {
+    execution.updateScore(1.0, 1)
+
+    assertEquals(1.0, execution.score)
+    assertEquals(1, execution.destroyedAsteroids)
+  }
+
+  @Test
+  fun `it adds to Scoreboard json`() {
+    val file = File("src/main/kotlin/galaxyraiders/core/score/Scoreboard.json")
+    execution.addToScoreboard()
+
+    assertNotEquals(file.length(), 0)
+  }
+
+  @Test
+  fun `it adds the updated Scoreboard json`() {
+    val file = File("src/main/kotlin/galaxyraiders/core/score/Scoreboard.json")
+    execution.updateScore(1.0, 1)
+    execution.updateScore(2.0, 2)
+    execution.updateScore(11.0, 101)
+    execution.addToScoreboard()
+
+    assertNotEquals(file.length(), 0)
+    assertEquals(11.0, execution.score)
+    assertEquals(101, execution.destroyedAsteroids)
+  }
+}
